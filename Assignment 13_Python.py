ec2ee8c1e0f40f571d03d0dee9de2675121fb5c4
MedhaVashisth <112161904+MedhaVashisth@users.noreply.github.com>
2022-09-16 16:25:15 +0530

    Add files via upload

---
+++ Assignment 13_Python.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Excel is more superior when compared with the CSV file format; thus, CSV consumes less amount of file size when the user is importing data; it is a much faster format compared to excel. CSV does not manipulate data and stores it as-is. Excel also allows the user the add-in feature.
+2. You pass a File object, obtained from a call to open().
+3. File objects need to be opened in read-binary ('rb') for Reader objects and write-binary ('wb') for Writer objects.
+4. The writerow() method.
+5. The delimiter argument changes the string used to separate cells in a row. The lineterminator argument changes the string used to separate rows.
+6. json.loads()
+7. json.dumps()
